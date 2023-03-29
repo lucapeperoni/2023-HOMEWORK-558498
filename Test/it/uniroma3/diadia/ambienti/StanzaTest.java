fefd53c9e376c8cec7c777a1b2a193b8504c584f
lucapeperoni <luc.peperoni@stud.uniroma3.it>
2023-03-29 17:21:13 +0200

    Primo homework finito + test di fragale

--- Test/it/uniroma3/diadia/ambienti/StanzaTest.java
+++ Test/it/uniroma3/diadia/ambienti/StanzaTest.java
@@ -1,36 +1,92 @@
 package it.uniroma3.diadia.ambienti;
-import it.uniroma3.diadia.attrezzi.Attrezzo;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 
 class StanzaTest {
-	private Stanza stanzaTest;
-	private Attrezzo clava;
-	private Attrezzo lanterna;
-	
+
+	private static final int MAX_ATTREZZI = 10;
+	private static final String ATTREZZO = "AttrezzoDiTest";
+	private static final String STANZA = "StanzaTest";
+	private static final String STANZA_ADIACENTE = "StanzaAdiacente";
+	private static final String NORD = "nord";
+
+	protected Stanza stanza;
+
 	@BeforeEach
-	public void setUp() {
-		this.stanzaTest = new Stanza("stanzaTest");
-		this.clava = new Attrezzo("clava", 1);
-		this.lanterna = new Attrezzo("lanterna", 3);
+	void setUp() {
+		this.stanza=new Stanza(STANZA);
+	}
+	//Metodo di utilità
+	private Stanza creaStanzaEImpostaAdiacente (Stanza stanzaDiPartenza, String nomeStanzaAdiacente, String direzione) {
+		Stanza stanzaAdiacente = new Stanza(nomeStanzaAdiacente);
+		stanzaDiPartenza.impostaStanzaAdiacente(direzione,stanzaAdiacente);
+		return stanzaAdiacente;
+	}
+
+	@Test
+	void testImpostaStanzaAdiacente() {
+		Stanza adiacente = creaStanzaEImpostaAdiacente(this.stanza, STANZA_ADIACENTE, NORD);
+		assertEquals(adiacente, this.stanza.getStanzaAdiacente(NORD));
+	}
+
+	@Test
+	void testGetStanzaAdiacenteEsistente() {
+		Stanza adiacente = new Stanza(STANZA_ADIACENTE);
+		stanza.impostaStanzaAdiacente(NORD, adiacente);
+		assertEquals(adiacente,stanza.getStanzaAdiacente(NORD));
+	}
+
+	@Test
+	public void testGetStanzaAdiacenteInesistente() {
+		assertNull(stanza.getStanzaAdiacente(NORD));
 	}
 	@Test
-	void testAddAttrezzo() {
-		assertEquals(true, stanzaTest.addAttrezzo(clava));
+	public void testGetStanzaAdiacenteDirezioneNonValida() {
+		String direzioneNonValida= "direzioneACaso";
+		Stanza adiacente = new Stanza(STANZA_ADIACENTE);
+		stanza.impostaStanzaAdiacente(NORD, adiacente);
+		assertNull(stanza.getStanzaAdiacente(direzioneNonValida));
 	}
+
 	@Test 
-	void testRemoveAttrezzo(){
-		assertEquals(true, stanzaTest.removeAttrezzo(clava));
+	public void testGetAttrezzoEsistente() {
+		Attrezzo esistente = new Attrezzo(ATTREZZO,1);
+		stanza.addAttrezzo(esistente);
+		assertEquals(esistente,stanza.getAttrezzo(ATTREZZO));
 	}
-	@Test 
-	void testHasAttrezzo(){
-		assertEquals (false, stanzaTest.hasAttrezzo("clava"));
+
+	@Test
+	public void testGetAttrezzoNonEsistente() {
+		assertNull(stanza.getAttrezzo(ATTREZZO));
 	}
-	
+
+	@Test
+	public void testGetAttrezzoNomeVuoto() {
+		Attrezzo attrezzo = new Attrezzo (ATTREZZO,1);
+		stanza.addAttrezzo(attrezzo);
+		assertNull(stanza.getAttrezzo(""));
+	}
+
+	// controlla se il metodo aggiunge correttamente l'attrezzo
+	@Test
+	public void testAddAttrezzo() {
+		Attrezzo attrezzo = new Attrezzo(ATTREZZO,10);
+		assertEquals(true, stanza.addAttrezzo(attrezzo));
+	}
+
+	// controlla se il metodo non aggiunge altri attrezzi se la stanza è piena
+	@Test
+	public void testAddAttrezzoArrayPieno() {
+		Attrezzo attrezzo = new Attrezzo("osso",10);
+		for(int i = 0;i < MAX_ATTREZZI;i++)
+			stanza.addAttrezzo(attrezzo);
+		assertFalse(stanza.addAttrezzo(attrezzo));
+	}
 
 }
