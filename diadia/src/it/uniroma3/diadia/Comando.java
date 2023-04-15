10b5fd4dd59c856ccfccfd81d031bd77c93ce2fc
lucapeperoni <luc.peperoni@stud.uniroma3.it>
2023-04-15 16:26:59 +0200

    finale pre secondo esonero, implementato roba studio di caso delle slide del prof

--- diadia/src/it/uniroma3/diadia/Comando.java
+++ diadia/src/it/uniroma3/diadia/Comando.java
@@ -1,45 +1,15 @@
 package it.uniroma3.diadia;
 
-import java.util.Scanner;
+public interface Comando {
 
-/**
- * Questa classe modella un comando.
- * Un comando consiste al piu' di due parole:
- * il nome del comando ed un parametro
- * su cui si applica il comando.
- * (Ad es. alla riga digitata dall'utente "vai nord"
- *  corrisponde un comando di nome "vai" e parametro "nord").
- *
- * @author  docente di POO
- * @version base
- */
+	/**
+	 * esecuzione del comando
+	 */
+	public void esegui(Partita partita);
 
-public class Comando {
+	/**
+	 * set parametro del comando
+	 */
+	public void setParametro(String parametro);
 
-    private String nome;
-    private String parametro;
-
-    public Comando(String istruzione) {
-		Scanner scannerDiParole = new Scanner(istruzione);
-
-		// prima parola: nome del comando
-		if (scannerDiParole.hasNext())
-			this.nome = scannerDiParole.next(); 
-
-		// seconda parola: eventuale parametro
-		if (scannerDiParole.hasNext())
-			this.parametro = scannerDiParole.next();
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public String getParametro() {
-        return this.parametro;
-    }
-
-    public boolean sconosciuto() {
-        return (this.nome == null);
-    }
 }
--- diadia/src/it/uniroma3/diadia/DiaDia.java
+++ diadia/src/it/uniroma3/diadia/DiaDia.java
@@ -59,30 +59,19 @@ public class DiaDia {
 	 * @return true se l'istruzione e' eseguita e il gioco continua, false altrimenti
 	 */
 	private boolean processaIstruzione(String istruzione) {
-		Comando comandoDaEseguire = new Comando(istruzione);
-		if(istruzione.length() > 0) {
-			if (comandoDaEseguire.getNome().equals("fine")) {
-				this.fine(); 
-				return true;
-			} else if (comandoDaEseguire.getNome().equals("vai"))
-				this.vai(comandoDaEseguire.getParametro());
-			else if (comandoDaEseguire.getNome().equals("prendi"))
-				this.prendi(comandoDaEseguire.getParametro());
-			else if (comandoDaEseguire.getNome().equals("posa"))
-				this.posa(comandoDaEseguire.getParametro());
-			else if (comandoDaEseguire.getNome().equals("aiuto"))
-				this.aiuto();
-			else
-				io.mostraMessaggio("Comando sconosciuto");
-			if (this.partita.vinta()) {
-				io.mostraMessaggio("Hai vinto!");
-				return true;
-			} else
-				return false;
-		}  
-		else 
-			return false;
-	}
+		Comando comandoDaEseguire;
+		FabbricaDiComandiFisarmonica factory = new FabbricaDiComandiFisarmonica();
+		comandoDaEseguire = factory.costruisciComando(istruzione);
+		comandoDaEseguire.esegui(this.partita);
+		if (this.partita.vinta())
+
+		System.out.println("Hai vinto!");
+		if (!this.partita.giocatoreIsVivo())
+
+		System.out.println("Hai esaurito i CFU...");
+
+		return this.partita.isFinita();
+		}
 
 	// implementazioni dei comandi dell'utente:
 
